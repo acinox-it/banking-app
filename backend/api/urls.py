e8bd171fc767c3acf6a858b1b0ac21667328945c
acinox-it <sahraoui.abdeldjalil@outlook.com>
2025-11-02 03:21:58 +0100

    Add initial API structure with models, views, admin, and routing

---
+++ backend/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from accounts.views import UserViewSet
+from transactions.views import BankAccountViewSet, TransactionViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+# Router setup
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'accounts', BankAccountViewSet, basename='bankaccount')
+router.register(r'transactions', TransactionViewSet, basename='transaction')
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
